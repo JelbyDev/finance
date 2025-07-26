ec2c1e96e25981a84fc0648b22a832d4addf0c51
Jelby <evgeniy.ipatov92@mail.ru>
2025-07-26 15:47:55 +0300

    feat(ESLint): Добавил правило для порядка атрибутов в компонентах

--- eslint.config.ts
+++ eslint.config.ts
@@ -18,6 +18,31 @@ export default defineConfigWithVueTs(
       'vue/padding-line-between-blocks': 'error',
       //Запрещает пустые секции
       'vue/no-empty-component-block': 'error',
+      // Порядок атрибутов в компонентах
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'DEFINITION', // 'is', 'v-is'
+            'LIST_RENDERING', // 'v-for item in items'
+            'CONDITIONALS', // 'v-if', 'v-else-if', 'v-else', 'v-show', 'v-cloak'
+            'RENDER_MODIFIERS', // 'v-once', 'v-pre'
+            'TWO_WAY_BINDING', // 'v-model'
+            'GLOBAL', // 'id'
+            'UNIQUE', // 'ref', 'key'
+            'SLOT', // 'v-slot', 'slot'
+            // Секция OTHER_ATTR, обычные атрибуты (class, style и т.д.)
+            // можно поставить либо ее либо задать отдельно [ATTR_DYNAMIC, ATTR_STATIC, ATTR_SHORTHAND_BOOL]
+            'ATTR_STATIC', // 'prop="foo"', 'custom-prop="foo"'
+            'ATTR_DYNAMIC', // 'v-bind:prop="foo"', ':prop="foo"'
+            'ATTR_SHORTHAND_BOOL', // 'boolean-prop'
+            'CONTENT', // 'v-text', 'v-html'
+            'OTHER_DIRECTIVES', // 'v-custom-directive'
+            'EVENTS', // '@click="functionCall"', 'v-on="event"'
+          ],
+          alphabetical: false, // не сортировать атрибуты внутри групп по алфавиту
+        },
+      ],
       // ------ ОТРЕЗОК ДЛЯ .VUE ФАЙЛОВ --- OFF ---
     },
   },
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
   import { RouterView } from 'vue-router';
+
+  const ff = ref('1');
 </script>
 
 <template>
   <div>Заглушка</div>
+
+  <div v-if="true" class="123" :style="`height: ${ff}`" :class="ff" v-text="ff" />
   <RouterView />
 </template>
